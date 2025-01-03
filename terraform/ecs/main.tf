7630a333c0b05d07ab5e47ae9eb126b180776469
Alexandre Paiva <alexandremarcio1504@gmail.com>
2025-01-03 11:46:53 -0300

    funcional bg automatico

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -37,6 +37,13 @@ resource "aws_security_group" "ecs_node_sg" {
   name_prefix = "demo-ecs-node-sg-"
   vpc_id      = var.vpc_id
 
+  ingress {
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
+    security_groups = [aws_security_group.http.id]
+  }
+
   egress {
     from_port   = 0
     to_port     = 65535
