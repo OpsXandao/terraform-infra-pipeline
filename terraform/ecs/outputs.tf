c0c1b2ad6774782f68b3100f12c2502d86c4fe51
Alexandre Paiva <alexandremarcio1504@gmail.com>
2025-01-07 11:24:16 -0300

    outputs arns

--- terraform/ecs/outputs.tf
+++ terraform/ecs/outputs.tf
@@ -1,3 +1,11 @@
 output "alb_url" {
   value = aws_lb.main.dns_name
 }
+
+output "ecs_exec_role_arn" {
+  value = aws_iam_role.ecs_exec_role.arn
+}
+
+output "ecs_task_role_arn" {
+  value = aws_iam_role.ecs_task_role.arn
+}
