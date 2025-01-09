3fe0432fb4fe4b6400b8a53fbe989c58f3956354
Alexandre Paiva <alexandremarcio1504@gmail.com>
2025-01-09 17:42:22 -0300

    bg 2.2.4

--- terraform/ambiente/prd/ecs/main.tf
+++ terraform/ambiente/prd/ecs/main.tf
@@ -141,6 +141,7 @@ resource "aws_ecs_service" "app" {
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.app.arn
   desired_count   = 1
+  
    load_balancer {
     target_group_arn = var.aws_lb_target_group_blue_arn
     container_name   = "app" 
